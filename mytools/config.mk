c4d16cda2aa416ddf07edfc3552e6a480aa05ad4
Guilhem Lavaux <lavaux@illinois.edu>
2011-02-02 14:15:37 -0600

    Better void stacking options

--- mytools/config.mk
+++
@@ -1,6 +0,0 @@
-CC= gcc
-CXX= g++
-CPPFLAGS=  
-LDFLAGS= -L/usr/local/lib -lCosmoTool -lgsl -lgslcblas
-CXXFLAGS= $(CPPFLAGS) -ggdb -O0 -ftree-vectorize -ftree-vectorizer-verbose=2 -ffast-math
-CFLAGS= $(CPPFLAGS) -ggdb -O0 -ftree-vectorize -ftree-vectorizer-verbose=2 -ffast-math
