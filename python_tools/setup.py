e380538e660723cf832ae917f4161017707df685
Guilhem Lavaux <guilhem.lavaux@laposte.net>
2012-10-31 14:51:59 -0400

    Cleanup coding style

--- python_tools/setup.py
+++ python_tools/setup.py
@@ -8,7 +8,13 @@ setup(
     version='1.0',
     cmdclass = {'build_ext': build_ext},
     include_dirs = [np.get_include()],
-    packages=['void_python_tools','void_python_tools.backend','void_python_tools.apTools','void_python_tools.apTools.profiles','void_python_tools.apTools.chi2', 'void_python_tools.plotting'],
+    packages=
+       ['void_python_tools','void_python_tools.backend','void_python_tools.apTools',
+        'void_python_tools.apTools.profiles','void_python_tools.apTools.chi2', 'void_python_tools.plotting'],
     #ext_modules = [Extension("void_python_tools.chi2.velocityProfileFitNative", ["void_python_tools/chi2/velocityProfileFitNative.pyx"], libraries=["gsl", "gslcblas"]), Extension("void_python_tools.chi2.likelihoo", ["void_python_tools/chi2/likelihood.pyx"], libraries=["gsl", "gslcblas"])]
-    ext_modules = [Extension("void_python_tools.apTools.chi2.velocityProfileFitNative", ["void_python_tools/apTools/chi2/velocityProfileFitNative.pyx"], libraries=["gsl", "gslcblas"])]
+    ext_modules = [
+       Extension("void_python_tools.apTools.chi2.velocityProfileFitNative", 
+                 ["void_python_tools/apTools/chi2/velocityProfileFitNative.pyx"],
+       libraries=["gsl", "gslcblas"])
+    ]
 )
