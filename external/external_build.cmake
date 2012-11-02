25f2244d49ed2d0bdf217545530979cff4df8c97
Guilhem Lavaux <guilhem.lavaux@laposte.net>
2012-11-01 21:28:38 -0400

    Improved handling of openmp flags using set_target_properties

--- external/external_build.cmake
+++ external/external_build.cmake
@@ -5,12 +5,8 @@ OPTION(ENABLE_OPENMP "Set to Yes if Healpix and/or you need openMP" OFF)
 IF(ENABLE_OPENMP)
 
   IF (NOT OPENMP_FOUND)
-    MESSAGE(ERROR "No known compiler option for enabling OpenMP")
+    MESSAGE(FATAL_ERROR "No known compiler option for enabling OpenMP")
   ENDIF(NOT OPENMP_FOUND)
-
-  SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OpenMP_C_FLAGS}")
-  SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
-  SET(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKED_FLAGS} ${OpenMP_C_FLAGS}")
 
 ENDIF(ENABLE_OPENMP)
 
