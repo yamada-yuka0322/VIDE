971cf21ea2b56f248bbc8e02a0b69149117d9f1d
P.M. Sutter <sutter@iap.fr>
2012-11-15 10:11:51 -0600

    potential fix so that end of prunevoids is correctly detected

--- python_tools/void_python_tools/backend/launchers.py
+++ python_tools/void_python_tools/backend/launchers.py
@@ -297,7 +297,7 @@ def launchPrune(sample, binPath, thisDataPortion=None,
     cmd += " >& " + logFile
     os.system(cmd)
 
-    if jobSuccessful(logFile, "NetCDF: Not a valid ID\n") or jobSuccessful(logFile, "Done!"):
+    if jobSuccessful(logFile, "NetCDF: Not a valid ID\n") or jobSuccessful(logFile, "Done!") or jobSuccessful(logFile, ""):
       print "done"
     else:
       print "FAILED!"
