bce9ac1dd364614f3d9925a419c4605cd3d12f14
Guilhem Lavaux <guilhem.lavaux@laposte.net>
2013-03-02 18:33:33 -0500

    Removed the possibility of a later version. The patent mess is unclear.

--- c_tools/mock/loaders/sdf_loader.cpp
+++ c_tools/mock/loaders/sdf_loader.cpp
@@ -5,8 +5,7 @@
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+    the Free Software Foundation; version 2 of the License.
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -17,6 +16,7 @@
     with this program; if not, write to the Free Software Foundation, Inc.,
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 +*/
+
 
 #include <iostream>
 #include <boost/format.hpp>
