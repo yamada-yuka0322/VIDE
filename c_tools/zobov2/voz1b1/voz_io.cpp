1553601028c119afe178521c893d4436b63841be
Guilhem Lavaux <lavaux@iap.fr>
2013-06-11 10:48:04 +0200

    Initialize volume  of the box.

--- c_tools/zobov2/voz1b1/voz_io.cpp
+++ c_tools/zobov2/voz1b1/voz_io.cpp
@@ -54,12 +54,14 @@ void PositionData::findExtrema()
       xyz_max[i] = -BF;
     }
 
+  V0=1;
   for (int i = 0; i < 3; i++)
     {
       for (pid_t p = 0; p < np; p++)
         {
-          xyz_min[p] = min(xyz_min[p], xyz[p][i]);
-          xyz_max[p] = max(xyz_max[p], xyz[p][i]);
+          xyz_min[i] = min(xyz_min[i], xyz[i][p]);
+          xyz_max[i] = max(xyz_max[i], xyz[i][p]);
         }
+      V0 *= (xyz_max[i]-xyz_min[i]);
     }
 }
