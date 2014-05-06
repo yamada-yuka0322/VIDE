b441758a646c844e01e98e0c66e918427d172b06
P.M. Sutter <sutter@iap.fr>
2014-05-06 14:05:20 -0500

    fixed python setup

--- python_tools/setup.py
+++ python_tools/setup.py
@@ -34,9 +34,9 @@ setup(
        ['void_python_tools','void_python_tools.backend','void_python_tools.apTools', 'void_python_tools.xcor', 'void_python_tools.partUtil',
         'void_python_tools.apTools.profiles','void_python_tools.apTools.chi2', 'void_python_tools.plotting'],
     #ext_modules = [Extension("void_python_tools.chi2.velocityProfileFitNative", ["void_python_tools/chi2/velocityProfileFitNative.pyx"], libraries=["gsl", "gslcblas"]), Extension("void_python_tools.chi2.likelihoo", ["void_python_tools/chi2/likelihood.pyx"], libraries=["gsl", "gslcblas"])]
-    ext_modules = [
-       Extension("void_python_tools.apTools.chi2.velocityProfileFitNative", 
-                 ["void_python_tools/apTools/chi2/velocityProfileFitNative.pyx"],
-       libraries=["gsl", "gslcblas"], library_dirs=[VOID_GSL+"/lib"], include_dirs=[VOID_GSL+"/include"])
-    ]
+    #ext_modules = [
+    #   Extension("void_python_tools.apTools.chi2.velocityProfileFitNative", 
+    #             ["void_python_tools/apTools/chi2/velocityProfileFitNative.pyx"],
+    #   libraries=["gsl", "gslcblas"], library_dirs=[VOID_GSL+"/lib"], include_dirs=[VOID_GSL+"/include"])
+    #]
 )
