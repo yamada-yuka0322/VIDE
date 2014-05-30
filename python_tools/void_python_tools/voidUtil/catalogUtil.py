b2d5c05286a4acf389a9cb0d5f6f894fe1d6409a
P.M. Sutter <sutter@iap.fr>
2014-05-30 09:22:34 -0400

    fixed array bug

--- python_tools/void_python_tools/voidUtil/catalogUtil.py
+++ python_tools/void_python_tools/voidUtil/catalogUtil.py
@@ -461,7 +461,7 @@ def getArray(objectList, attr):
       attrArr[:,idim] = np.fromiter((np.atleast_1d(getattr(v, attr))[idim] \
                                     for v in objectList), float )
   
-    if ndim == 1: attrArr = attArr[:,0]
+    if ndim == 1: attrArr = attrArr[:,0]
 
     return attrArr
   else:
