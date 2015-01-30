7a60b0105d060371ca6fff4a66ed9d0ad385d8a2
P.M. Sutter <sutter@iap.fr>
2015-01-30 05:08:39 -0600

    point to correct voz1b1

--- zobov/vozinit.c
+++ zobov/vozinit.c
@@ -173,7 +173,7 @@ int main(int argc, char *argv[]) {
   for (b[0]=0;b[0]<numdiv; b[0]++) {
    for (b[1] = 0; b[1] < numdiv; b[1]++) {
     for (b[2] = 0; b[2] < numdiv; b[2]++) {
-      fprintf(scr,"%s/../c_tools/zobov2/voz1b1/voz1b1 %s %f %f %s %d %d %d %d %s&\n",
+      fprintf(scr,"%s/../c_tools/zobov2/voz1b1/voz1b1_2 %s %f %f %s %d %d %d %d %s&\n",
         vobozPath,
 	      posfile,border,boxsize,suffix,numdiv,b[0],b[1],b[2], 
         outDir);
